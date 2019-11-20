be40b2daf68fde3c82d3eeed1a6714cdbce0a32c
Kris <kriskuiper74@gmail.com>
2019-11-20 09:42:59 +0100

    feat(categories): map termmaster to category

---
+++ src/lib/categories.js
@@ -0,0 +1,6 @@
+export default {
+	'wapens': 'https://hdl.handle.net/20.500.11840/termmaster2091',
+	'kleding': 'https://hdl.handle.net/20.500.11840/termmaster2705',
+	'jacht, visserij en voedselgaring': 'https://hdl.handle.net/20.500.11840/termmaster2803',
+	'nijverheid, handel en diensverlening': 'https://hdl.handle.net/20.500.11840/termmaster2754'
+}

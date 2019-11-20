1f887384f31429ccba8df8b5bbd51400c5da820c
Kris <kriskuiper74@gmail.com>
2019-11-20 09:44:10 +0100

    feat(app): implement dynamic query

--- src/app.js
+++ src/app.js
@@ -1,8 +1,9 @@
+import categories from './lib/categories'
 import getCleanData from './lib/get-clean-data'
 
 // Have to use an iife here because we can't use await without async
 (async () => {
-	const data = await getCleanData()
+	const data = await getCleanData(categories['wapens'])
 	console.log('Works!')
 	console.log(data)
 })()

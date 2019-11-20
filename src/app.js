7c5f9f77690c9fc1bfee3c455338beb8e3583bab
Kris <kriskuiper74@gmail.com>
2019-11-20 13:12:40 +0100

    feat(app): implement mapbox

--- src/app.js
+++ src/app.js
@@ -1,9 +1,16 @@
 import categories from './lib/categories'
 import getCleanData from './lib/get-clean-data'
+import mapboxgl from 'mapbox-gl/dist/mapbox-gl.js'
 
 // Have to use an iife here because we can't use await without async
 (async () => {
-	const data = await getCleanData(categories['wapens'])
-	console.log('Works!')
+	mapboxgl.accessToken = 'pk.eyJ1Ijoia3Jpc2t1aXBlciIsImEiOiJjazM3NDltbGUwODJrM2RsaTJlZXk5OXJyIn0.L2lL-Iwyb9Nfip_C9G3v2w'
+
+	const map = new mapboxgl.Map({
+		container: 'map',
+		style: 'mapbox://styles/mapbox/light-v9',
+	})
+
+	const data = await getCleanData(categories['kleding'])
 	console.log(data)
 })()

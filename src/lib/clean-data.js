8f9d692ddefc64e7037cb5a217cd04bb12581836
Kris <kriskuiper74@gmail.com>
2019-11-20 09:41:32 +0100

    feat(clean-data): make it simpler

--- src/lib/clean-data.js
+++ src/lib/clean-data.js
@@ -1,20 +1,10 @@
-import formatYearForResult from './formatters/format-year-for-result'
-import formatRangeForResult from './formatters/format-range-for-result'
-
 export default function(data) {
 	return data
 		.map(item => ({
 			title: item.title && item.title.value,
-			year: item.year && formatYearForResult(item.year.value),
 			religion: item.religionLabel && item.religionLabel.value,
 			country: item.countryLabel && item.countryLabel.value,
 			lat: item.lat && item.lat.value,
 			long: item.long && item.long.value
 		}))
-		.filter(item => !isNaN(item.year))
-		.map(item => ({
-			...item,
-			range: formatRangeForResult(item.year)
-		}))
 }
-

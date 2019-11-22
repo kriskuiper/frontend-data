f3efcebf4bea81250ba5be4eef9b15452d781101
Kris <kriskuiper74@gmail.com>
2019-11-22 14:03:37 +0100

    feat(get-geo-countries): format to object

--- src/lib/get-geo-countries.js
+++ src/lib/get-geo-countries.js
@@ -5,10 +5,23 @@ export default function() {
 		const csvPath = '../../../data/geo-countries.csv'
 
 		return csv(csvPath)
-			.then(resolve)
+			.then(json => {
+				return resolve(formatObjectForCountries(json))
+			})
 			.catch(error => {
 				console.error(error)
 				reject(`Could not parse ${csvPath} to json`)
 			})
 	})
+}
+
+function formatObjectForCountries(countries) {
+	return countries.reduce((formatted, currentCountry) => {
+		formatted[currentCountry.name] = {
+			lat: currentCountry.latitude,
+			long: currentCountry.longitude
+		}
+
+		return formatted
+	}, {})
 }

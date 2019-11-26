91bd21d827122b159a74fa10f88df55649b737ac
Kris <kriskuiper74@gmail.com>
2019-11-26 13:16:39 +0100

    feat(app): put bar chart in own folder

--- src/app.js
+++ src/app.js
@@ -1,11 +1,12 @@
 import mapboxgl from 'mapbox-gl/dist/mapbox-gl.js'
-import { select, scaleLinear, max, axisBottom } from 'd3'
+import { select } from 'd3'
 import tip from 'd3-tip'
 
 import getCleanData from './lib/get-clean-data'
 import renderOptions from './lib/render-options'
 import getUniqueReligions from './lib/get-unique-religions'
 import transformReligionsForCountry from './lib/transformers/transform-religions-for-country'
+import renderBarChart from './lib/charts/bar-chart'
 import termmasters from '../data/termmasters'
 
 // Have to use an iife here because we can't use await without async
@@ -84,81 +85,5 @@ import termmasters from '../data/termmasters'
 		})
 	}
 
-	function renderBarChart(data) {
-		const isRendered = document.getElementById('bar-chart-container').dataset.rendered
-		const barChartConfig = {
-			width: 350,
-			height: 350,
-			padding: 20,
-			scale: calculateXScale({ padding: 20, width: 350 }, data),
-			ticks: 10,
-		}
 
-		return !isRendered
-			? renderInitialBarChart(barChartConfig, data)
-			: updateBarChart(barChartConfig, data)
-	}
-
-	function renderInitialBarChart(config, data) {
-		const helperText = document.getElementById('helper-text')
-		const barChartContainer = document.getElementById('bar-chart-container')
-		const svg = select(barChartContainer)
-			.append('svg')
-			.attr('width', config.width)
-			.attr('height', config.height)
-
-		config = {...config, svg}
-
-		// Set data-rendered as attribute to filters
-		barChartContainer.setAttribute('data-rendered', true)
-
-		// Remove the helper text
-		barChartContainer.removeChild(helperText)
-
-		// Create the bar chart
-		// addLabelsToBars(config, data)
-		addHorizontalAxis(config)
-		addBars(config, data)
-	}
-
-	function updateBarChart({ width, height }, data) {
-		console.log('updated')
-	}
 })()
-
-function calculateXScale({ padding, width }, data) {
-	console.log(padding)
-
-	return scaleLinear()
-		.domain([0, max(data, d => {
-			console.log('Data: ', d)
-			return d.amount
-		})])
-		.range([padding, width - padding])
-		.nice()
-}
-
-function addHorizontalAxis({ svg, scale, height, padding, ticks }) {
-	const horizontalAxis = axisBottom(scale).ticks(ticks)
-
-	return svg.append('g')
-		.attr('transform', `translate(0, ${height - padding})`)
-		.attr('class', 'bar-chart__x-axis')
-		.call(horizontalAxis)
-		.call(g => g.select('.domain').remove())
-}
-
-function addBars({ svg, scale, padding }, data) {
-	const barHeight = 40
-	const barSpacing = barHeight + 5
-
-	return svg.selectAll('rect')
-		.data(data)
-		.enter()
-		.append('rect')
-		.attr('x', 0)
-		.attr('y', (d, i) => i * barSpacing)
-		.attr('width', (d) => scale(d.amount))
-		.attr('height', barHeight)
-		.attr('class', 'bar-chart__bar')
-}

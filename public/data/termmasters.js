103eea8f0ded4cdef1ddad26268a11890224f6da
Kris <kriskuiper74@gmail.com>
2019-11-27 12:19:10 +0100

    chore: fix build config

---
+++ public/data/termmasters.js
@@ -0,0 +1,8 @@
+export default [
+	{ name: 'Kleding en persoonlijke versiering', value: 'https://hdl.handle.net/20.500.11840/termmaster2704' },
+	{ name: 'Nijverheid, handel en dienstverlening', value: 'https://hdl.handle.net/20.500.11840/termmaster2754' },
+	{ name: 'Jacht, visserij en voedselgaring', value: 'https://hdl.handle.net/20.500.11840/termmaster2803' },
+	{ name: 'Land, tuin en bosbouw', value: 'https://hdl.handle.net/20.500.11840/termmaster2819' },
+	{ name: 'Veeteelt en producten', value: 'https://hdl.handle.net/20.500.11840/termmaster1843' },
+	{ name: 'Vervoer', value: 'https://hdl.handle.net/20.500.11840/termmaster2624' }
+]

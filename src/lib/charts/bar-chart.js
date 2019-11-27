ea7cdc28f3dffec56eac2c6140b65a3e3f4247e7
Kris <kriskuiper74@gmail.com>
2019-11-27 11:48:52 +0100

    feat(bar-chart): render and update bar chart

--- src/lib/charts/bar-chart.js
+++ src/lib/charts/bar-chart.js
@@ -1,78 +1,85 @@
 import { select, scaleLinear, max, axisBottom } from 'd3'
 
+/**
+	Thanks to Nicholas H: https://bl.ocks.org/syncopika/f1c9036b0deb058454f825238a95b6be
+	You can read all about how I implemented this example in the Wiki: (link to wiki)
+
+	@description The function gets data and renders or updates the barchart based on the 'data-rendered'
+	attribute of the barchart container.
+
+	@param {array} data An array of data objects
+	@return {object} A D3 object with the updated barchart
+*/
+
 export default function(data) {
-	const isRendered = document.getElementById('bar-chart-container').dataset.rendered
-	const barChartConfig = {
+	const container = document.getElementById('bar-chart-container')
+	const isRendered = container.dataset.rendered
+
+	const config = {
+		container,
 		width: 350,
 		height: 350,
-		padding: 20,
-		scale: calculateXScale({ padding: 20, width: 350 }, data),
-		ticks: 10,
+		margin: {
+			top: 20,
+			right: 20,
+			bottom: 40,
+			left: 20
+		},
+		scaleX: scaleLinear()
+			// Actually this has to be the config's width, but now it's hardcoded so it works for now
+			// have to refactor this in the future though...
+			.range([0, 350])
 	}
 
 	return !isRendered
-		? renderInitialBarChart(barChartConfig, data)
-		: updateBarChart(barChartConfig, data)
+		? renderBarChart(config, data)
+		: updateBarChart(config.scaleX, data)
 }
 
-function renderInitialBarChart(config, data) {
+function renderBarChart({ container, width, height, margin, scaleX }, data) {
 	const helperText = document.getElementById('helper-text')
-	const barChartContainer = document.getElementById('bar-chart-container')
-	const svg = select(barChartContainer)
+
+	container.removeChild(helperText)
+	container.setAttribute('data-rendered', true)
+
+	const barChart = select(container)
+		.attr('width', width)
+		.attr('height', height)
 		.append('svg')
-		.attr('width', config.width)
-		.attr('height', config.height)
+		.attr('class', 'bar-chart')
+		.attr('transform', `translate(0, ${margin.top})`)
 
-	config = {...config, svg}
+	const xAxis = axisBottom(scaleX)
 
-	// Set data-rendered as attribute to filters
-	barChartContainer.setAttribute('data-rendered', true)
-
-	// Remove the helper text
-	barChartContainer.removeChild(helperText)
-
-	// Create the bar chart
-	addHorizontalAxis(config)
-	addBars(config, data)
-}
-
-function updateBarChart({ width, height }, data) {
-	console.log('updated')
-}
-
-function calculateXScale({ padding, width }, data) {
-	console.log(padding)
-
-	return scaleLinear()
-		.domain([0, max(data, d => {
-			console.log('Data: ', d)
-			return d.amount
-		})])
-		.range([padding, width - padding])
-		.nice()
-}
-
-function addHorizontalAxis({ svg, scale, height, padding, ticks }) {
-	const horizontalAxis = axisBottom(scale).ticks(ticks)
-
-	return svg.append('g')
-		.attr('transform', `translate(0, ${height - padding})`)
+	barChart.append('g')
 		.attr('class', 'bar-chart__x-axis')
-		.call(horizontalAxis)
-		.call(g => g.select('.domain').remove())
+		.attr('transform', `translate(0, ${height})`)
+		.call(xAxis)
+
+	return updateBarChart(scaleX, data)
 }
 
-function addBars({ svg, scale, padding }, data) {
-	const barHeight = 40
+function updateBarChart(scaleX, data) {
+	scaleX.domain([0, max(data, d => d.amount)])
+
+	const barHeight = 20
 	const barSpacing = barHeight + 5
 
-	return svg.selectAll('rect')
+	return select('.bar-chart').selectAll('rect')
 		.data(data)
-		.enter()
-		.append('rect')
-		.attr('x', 0)
-		.attr('y', (d, i) => i * barSpacing)
-		.attr('width', (d) => scale(d.amount))
-		.attr('height', barHeight)
-		.attr('class', 'bar-chart__bar')
+		.join(
+			enter => {
+				return enter.append('rect')
+					.attr('x', 0)
+					.attr('y', (d, i) => i * barSpacing)
+					.attr('width', (d) => scaleX(d.amount))
+					.attr('height', barHeight)
+					.attr('class', 'bar-chart__bar')
+			},
+			update => {
+				return update.select('rect')
+					.attr('width', (d) => scaleX(d.amount))
+			},
+			exit => exit.remove()
+		)
 }
